81ac5b517106626252a0c578cf94abe74ff8a1b3
badgersnax <berretterry@gmail.com>
2015-11-13 15:43:29 -0800

    script change

--- halo.sh
+++ halo.sh
@@ -1,2 +1,3 @@
-f [ -z {$HALO_TAG+x}]; then /opt/cloudpassage/bin/configure --agent-key=$HALO_KEY; else /opt/cloudpassage/bin/configure --agent-key=$HALO_KEY --tag=$HALO_TAG --read-only=true;fi
+#!/bin/sh
+if [ -z {$HALO_TAG+x}]; then /opt/cloudpassage/bin/configure --agent-key=$HALO_KEY; else /opt/cloudpassage/bin/configure --agent-key=$HALO_KEY --tag=$HALO_TAG --read-only=true;fi
 /etc/init.d/cphalod start
